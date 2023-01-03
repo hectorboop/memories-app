bc4cadcaec2262389ab5e56362055d629c3d72ea
Rushane WIlson <rushwilson1@gmail.com>
2023-01-03 12:21:05 -0500

    Fixed Dependencies

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const devEnv = process.env.NODE_ENV !== 'production';
+
+const { REACT_APP_DEV, REACT_APP_PROD } = process.env;
+
+const url = `${devEnv ? REACT_APP_DEV : REACT_APP_PROD}/posts`;
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const updatePost = (id, updatedPost) =>
+  axios.patch(`${url}/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotev from 'dotenv';
+
+dotev.config();
 
 import postRoutes from './routes/posts.js';
 
@@ -13,8 +16,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL =
-  'mongodb+srv://admin:DaVQEtTOjamfJbai@cluster0.tgourpc.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_STRING;
 const PORT = process.env.PORT || 5000;
 
 mongoose
